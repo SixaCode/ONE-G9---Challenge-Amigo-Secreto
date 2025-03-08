4436626e587024afa055c1174e827a0407410162
Brian Steven Gutierrez Prieto <mrjoer22@gmail.com>
2025-03-07 21:58:41 -0500

    Update friends list

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 function agregarAmigo () {
     let amigo = document.getElementById('amigo').value;
+    let listaAmigos = document.getElementById('listaAmigos');
 
     if (amigo == '') {
         alert("Por favor, inserte un nombre.");
     } else {
         amigos.push(amigo);
         document.getElementById('amigo').value = '';
+    }
+
+    listaAmigos.innerHTML = "";
+
+    for (i = 0; i < amigos.length; i++) {
+        let elementoLista = document.createElement("li");
+        let amigoAnadir = document.createTextNode(amigos[i]);
+        elementoLista.appendChild(amigoAnadir);
+        listaAmigos.appendChild(elementoLista);
     }
 }
 
